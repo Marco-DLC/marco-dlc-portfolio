c0bdc021c8c6c2921835df5440da5c441d0e3631
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-21 16:57:07 -0400

    Add opening section with a stylized animated title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './assets/components/Header.jsx'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Splash from "./components/Splash.jsx";
 
 function App() {
   return (
-    <div id='App'>
-     <Header />
+    <div id="App">
+      <Header />
+      <Splash />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Splash.jsx
@@ -0,0 +1,22 @@
+export default function Splash() {
+  return (
+    <div id="Splash">
+      <div className="wrapper">
+        <h1>
+          Front-
+          <br />
+          End
+          <br />
+          Developer
+        </h1>
+        <h1 className="text-effect">
+          Front-
+          <br />
+          End
+          <br />
+          Developer
+        </h1>
+      </div>
+    </div>
+  );
+}
