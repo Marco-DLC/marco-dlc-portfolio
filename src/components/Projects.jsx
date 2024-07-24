5e569dcdc1f349092fc387a020aab8cb79368052
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-24 18:00:34 -0400

    Add the rest of the Cards and their info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,9 +5,30 @@ export default function Projects() {
     imgUrl:
       "https://github.com/Marco-DLC/yeremiya-wright-vocalist/raw/main/public/yeremiyawright-screenshot01.png",
     siteUrl: "https://yerrywright.netlify.app/",
-    notice: "I'm still waiting for client's resumes and bio. This is why the resume buttons do nothing and the \"About Me\" contains dummy text.",
-    githubUrl: 'https://github.com/Marco-DLC/yeremiya-wright-vocalist',
-    tech: 'Vite + React',
+    notice:
+      "I'm still waiting for client's resumes and bio. This is why the resume buttons do nothing and the \"About Me\" contains dummy text.",
+    githubUrl: "https://github.com/Marco-DLC/yeremiya-wright-vocalist",
+    tech: "Vite + React",
+  };
+
+  const grimeyLimesProj = {
+    title: "grimey.limes",
+    subtitle: "Portfolio for an Online Digital Artist",
+    imgUrl:
+      "https://github.com/Marco-DLC/grimeylimes/raw/main/images/grimeylimes-screenshot02.png",
+    siteUrl: "https://marco-dlc.github.io/grimeylimes/",
+    githubUrl: "https://github.com/Marco-DLC/grimeylimes",
+    tech: "HTML + Javascript",
+  };
+
+  const espersWorldProj = {
+    title: "Esper's World",
+    subtitle: "Note-taking App Stylized to a Friend's Liking",
+    imgUrl:
+      "https://github.com/Marco-DLC/espers-world/raw/main/public/images/espers-world-screenshot.png",
+    siteUrl: "https://espersworld.netlify.app/",
+    githubUrl: "https://github.com/Marco-DLC/espers-world?tab=readme-ov-file",
+    tech: "Vite + React",
   };
 
   return (
@@ -15,6 +36,8 @@ export default function Projects() {
       <h2>My Work</h2>
       <div className="container">
         <Card project={yerryWrightProj} />
+        <Card project={grimeyLimesProj} />
+        <Card project={espersWorldProj} />
       </div>
     </div>
   );
@@ -28,16 +51,23 @@ function Card({ project }) {
       <h3>{title}</h3>
       <h4>{subtitle}</h4>
       <hr />
-      <p className="tech">Made with <span>{tech}</span></p>
+      <p className="tech">
+        Made with <span>{tech}</span>
+      </p>
       <img src={imgUrl} />
-      {notice && <p><strong>Notice </strong>{notice}</p> }
+      {notice && (
+        <p>
+          <strong>Notice </strong>
+          {notice}
+        </p>
+      )}
       <div>
-      <a href={siteUrl} target="_blank">
-        <button className="site">Live Site</button>
-      </a>
-      <a href={githubUrl} target="_blank">
-        <button>Source Code</button>
-      </a>
+        <a href={siteUrl} target="_blank">
+          <button className="site">Live Site</button>
+        </a>
+        <a href={githubUrl} target="_blank">
+          <button>Source Code</button>
+        </a>
       </div>
     </div>
   );
