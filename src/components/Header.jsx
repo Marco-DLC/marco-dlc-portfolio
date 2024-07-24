9fb80a9f8d6eb595c71693a2856c94512f40f252
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-24 11:47:31 -0400

    Add state for checking initial load for animating
    The functionality of the previous commit was to check if the opening
    section was visible. With this commit, if the section is visible, the
    header title will expand, but if the user scrolls away, it condenses.
    I wanted there to be a delay on the expanding animation so that it
    triggers after the Splash animation was done. However, I didn't like the
    delay that was still present when users would re-trigger the animation
    upon the Splash component becoming visible again, so I set the
    initialLoad state for running the animation once with a delay.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useRef, useState } from "react";
 import Navigator from "./Navigator";
 
 export default function Header({ splashOnScreen }) {
+  const h1Ref = useRef();
+  const [initialLoad, setInitialLoad] = useState(true);
+
+  useEffect(() => {
+    const h1Element = h1Ref.current;
+
+    if (h1Element) {
+      h1Element.classList.remove("animate", "animate-reverse");
+
+      void h1Element.offsetWidth;
+
+      if (initialLoad) {
+        h1Element.classList.add('animate',"initial-animate");
+        setTimeout(() => {
+          h1Element.classList.remove("initial-animate");
+          setInitialLoad(false);
+        }, 3000);
+      } else {
+        if (splashOnScreen) {
+          h1Element.classList.add("animate");
+        } else {
+          h1Element.classList.add("animate-reverse");
+        }
+      }
+    }
+  }, [splashOnScreen]);
+
   return (
     <header id="Header">
       <div>
-        <h1 className={splashOnScreen ? "expanded" : "condensed"}>
+        <h1 ref={h1Ref}>
           <div className="title-main ">Marco D</div>
           <span className="span-e ">e</span>
           <div className="title-L ">L</div>
