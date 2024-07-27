8153f1964304439ca13fb3791f638f1981b32ed7
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-07-27 11:25:54 -0400

    Add thank you message on contact form submit

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,45 +1,53 @@
-export default function ContactMe() {
+import { useState } from "react";
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-      
-        const myForm = event.target;
-        const formData = new FormData(myForm);
-        
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString(),
-        })
-          .then(() => alert("Thank you for your submission"))
-          .catch((error) => alert(error));
-      };
-      
-      document
-        .querySelector("form")
-        .addEventListener("submit", handleSubmit);
+export default function ContactMe() {
+  const [isSent, setIsSent] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => setIsSent(true))
+      .catch((error) => alert(error));
+  };
+
+  document.querySelector("form").addEventListener("submit", handleSubmit);
       
   return (
     <div id="ContactMe">
       <h2>Contact Me</h2>
-      <form name="contact" action={handleSubmit} method="POST">
-      <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label htmlFor="name" >Name: </label>
-          <input type="text" id='name' name="name" />
-        </p>
-        <p>
-          <label htmlFor="email">Email: </label>
-          <input type="email" id='email' name="email" />
-        </p>
-        <p>
-          <label htmlFor="message">Message: </label>
-          <textarea id='message' name="message" ></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      {isSent ? (
+        <div>
+          <h3>Thank You!</h3>
+          <p>Your message has been sent.</p>
+        </div>
+      ) : (
+        <form name="contact" action={handleSubmit} method="POST">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" name="name" required />
+          </p>
+          <p>
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email" name="email" required />
+          </p>
+          <p>
+            <label htmlFor="message">Message: </label>
+            <textarea id="message" name="message" required></textarea>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      )}
     </div>
   );
 }
